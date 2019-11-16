df281c60da8b03f75bb0e5839ce904b987619892
Rodrigo Santos da Silva <rodrigosnt3@gmail.com>
2019-11-16 17:35:49 -0300

    iniciando a lista de prolog e corrigindo algumas coisas no arquivo de familia

--- familia.pl
+++ familia.pl
@@ -2,6 +2,9 @@ father(roberval, rodrigo).
 father(roberval, vitoria).
 father(roberval, lorenzo).
 father(rubens, rosangela).
+father(rubens, rosival).
+father(rubens, roberval).
+father(rosival, ronald).
 
 mother(ivone, rodrigo).
 mother(marlene, lorenzo).
@@ -9,3 +12,12 @@ mother(marlene, vitoria).
 
 siblings(X, Y) :- father(Z, X) , father(Z, Y).
 siblings(X, Y) :- mother(Z, X) , mother(Z, Y).
+
+uncle(X, Y) :- siblings(X, Z) , father(Z, Y).
+uncle(X, Y) :- siblings(X, Z) , mother(Z, Y).
+
+aunt(X, Y) :- siblings(X, Z) , father(Z, Y).
+aunt(X, Y) :- siblings(X, Z) , mother(Z, Y).
+
+cousins(X, Y) :- uncle(Z, X) , father(Z, Y).
+cousins(X, Y) :- uncle(Z, X) , mother(Z, Y).
